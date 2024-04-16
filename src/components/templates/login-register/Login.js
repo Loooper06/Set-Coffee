8f7a6853c817ea94491b4ad3f93238c2550e1f35
Loooper06 <loooper06@gmail.com>
2024-04-16 16:25:44 +0330

    login register UI done

---
+++ src/components/templates/login-register/Login.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import styles from "./Login.module.css";
+
+const Login = () => {
+  return (
+    <>
+      <div className={styles.form}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="ایمیل/شماره موبایل"
+        />
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="رمز عبور"
+        />
+        <div className={styles.checkbox}>
+          <input type="checkbox" name="" id="" />
+          <p>مرا به یاد داشته باش</p>
+        </div>
+        <button className={styles.btn}>ورود</button>
+        <Link href={"/forget-password"} className={styles.forgot_pass}>
+          رمز عبور را فراموش کرده اید؟
+        </Link>
+        <button className={styles.btn}>ورود با کد یکبار مصرف</button>
+        <span>حساب کاربری ندارید؟</span>
+        <button className={styles.btn_light}>ثبت نام</button>
+      </div>
+      <Link href={"/"} className={styles.redirect_to_home}>
+        لغو
+      </Link>
+
+      {/* <Sms /> */}
+    </>
+  );
+};
+
+export default Login;
