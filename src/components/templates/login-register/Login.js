a39f35b9e2dfc60f5991e70c873ac9e6fc08a615
Loooper06 <loooper06@gmail.com>
2024-04-17 14:47:16 +0330

    login & register Done

--- src/components/templates/login-register/Login.js
+++ src/components/templates/login-register/Login.js
@@ -1,37 +1,54 @@
 import Link from "next/link";
 import styles from "./Login.module.css";
+import { useState } from "react";
+import Sms from "./Sms";
 
-const Login = () => {
+const Login = ({ showRegisterForm }) => {
+  const [isLoginWithOtp, setIsLoginWithOtp] = useState(false);
+
+  const hideOtpForm = () => setIsLoginWithOtp(false);
+
   return (
     <>
-      <div className={styles.form}>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="ایمیل/شماره موبایل"
-        />
-        <input
-          className={styles.input}
-          type="password"
-          placeholder="رمز عبور"
-        />
-        <div className={styles.checkbox}>
-          <input type="checkbox" name="" id="" />
-          <p>مرا به یاد داشته باش</p>
-        </div>
-        <button className={styles.btn}>ورود</button>
-        <Link href={"/forget-password"} className={styles.forgot_pass}>
-          رمز عبور را فراموش کرده اید؟
-        </Link>
-        <button className={styles.btn}>ورود با کد یکبار مصرف</button>
-        <span>حساب کاربری ندارید؟</span>
-        <button className={styles.btn_light}>ثبت نام</button>
-      </div>
-      <Link href={"/"} className={styles.redirect_to_home}>
-        لغو
-      </Link>
-
-      {/* <Sms /> */}
+      {!isLoginWithOtp ? (
+        <>
+          <div className={styles.form}>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="ایمیل/شماره موبایل"
+            />
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="رمز عبور"
+            />
+            <div className={styles.checkbox}>
+              <input type="checkbox" name="" id="" />
+              <p>مرا به یاد داشته باش</p>
+            </div>
+            <button className={styles.btn}>ورود</button>
+            <Link href={"/forget-password"} className={styles.forgot_pass}>
+              رمز عبور را فراموش کرده اید؟
+            </Link>
+            <button
+              className={styles.btn}
+              onClick={() => setIsLoginWithOtp(true)}
+            >
+              ورود با کد یکبار مصرف
+            </button>
+            <span>حساب کاربری ندارید؟</span>
+            <button className={styles.btn_light} onClick={showRegisterForm}>
+              ثبت نام
+            </button>
+          </div>
+          <Link href={"/"} className={styles.redirect_to_home}>
+            لغو
+          </Link>
+        </>
+      ) : (
+        <Sms hideOtpForm={hideOtpForm} />
+      )}
     </>
   );
 };
--- src/components/templates/login-register/Register.js
+++ src/components/templates/login-register/Register.js
@@ -1,38 +1,68 @@
+import { useState } from "react";
+import Link from "next/link";
 import styles from "./Register.module.css";
+import Sms from "./Sms";
 
-const Register = ({ registerWithPass = false }) => {
+const Register = ({ showloginForm }) => {
+  const [isRegisterWithOtp, setIsRegisterWithOtp] = useState(false);
+  const [registerWithPass, setRegisterWithPass] = useState(false);
+
+  const hideOtpForm = () => setIsRegisterWithOtp(false);
+
   return (
     <>
-      <div className={styles.form}>
-        <input className={styles.input} type="text" placeholder="نام" />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="شماره موبایل  "
-        />
-        <input
-          className={styles.input}
-          type="email"
-          placeholder="ایمیل (دلخواه)"
-        />
-        {registerWithPass && (
-          <input
-            className={styles.input}
-            type="password"
-            placeholder="رمز عبور"
-          />
-        )}
-        <button style={{ marginTop: "2rem" }} className={styles.btn}>
-          ثبت نام با کد تایید
-        </button>
-        <button style={{ marginTop: ".7rem" }} className={styles.btn}>
-          ثبت نام با رمزعبور
-        </button>
-        <p className={styles.back_to_login}>برگشت به ورود</p>
-      </div>
-      <p className={styles.redirect_to_home}>لغو</p>
-
-      {/* <Sms /> */}
+      {!isRegisterWithOtp ? (
+        <>
+          <div className={styles.form}>
+            <input className={styles.input} type="text" placeholder="نام" />
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="شماره موبایل  "
+            />
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="ایمیل (دلخواه)"
+            />
+            {registerWithPass && (
+              <input
+                className={styles.input}
+                type="password"
+                placeholder="رمز عبور"
+              />
+            )}
+            <button
+              style={{ marginTop: "2rem" }}
+              className={styles.btn}
+              onClick={() => {
+                setIsRegisterWithOtp(true);
+                setRegisterWithPass(false);
+              }}
+            >
+              ثبت نام با کد تایید
+            </button>
+            <button
+              style={{ marginTop: ".7rem" }}
+              className={styles.btn}
+              onClick={() => {
+                setRegisterWithPass(true);
+                setIsRegisterWithOtp(false);
+              }}
+            >
+              ثبت نام با رمزعبور
+            </button>
+            <p className={styles.back_to_login} onClick={showloginForm}>
+              برگشت به ورود
+            </p>
+          </div>
+          <Link href={"/"} className={styles.redirect_to_home}>
+            لغو
+          </Link>
+        </>
+      ) : (
+        <Sms hideOtpForm={hideOtpForm} />
+      )}
     </>
   );
 };
