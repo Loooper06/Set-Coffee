37d9c855c808b24113aa0296858fe510b5521c55
Loooper06 <loooper06@gmail.com>
2024-04-19 04:10:25 +0330

    product & comment Models Done

---
+++ models/Comment.js
@@ -0,0 +1,31 @@
+const { default: mongoose } = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    score: {
+      type: Number,
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.models.Comment || mongoose.model("Comment", schema);
+
+module.exports = model;
---
+++ models/Product.js
@@ -0,0 +1,53 @@
+const { default: mongoose } = require("mongoose");
+require("./Comment");
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    short_description: {
+      type: String,
+      required: false,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    suitable: {
+      type: String,
+      required: false,
+    },
+    smell: {
+      type: String,
+      required: false,
+    },
+    score: {
+      type: Number,
+      required: false,
+    },
+    tags: {
+      type: [String],
+      default: [],
+      required: true,
+    },
+    comments: {
+      type: [{ type: mongoose.Types.ObjectId, ref: "Comment" }],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.models.Product || mongoose.model("Product", schema);
+
+module.exports = model;
